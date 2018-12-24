b43d25925d1426637da748dea6d569877c308e6c
Orhan Hajderovic <orhanhajder@gmail.com>
2018-12-24 23:51:51 +0100

    update

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,8 @@
 // const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
 
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to mongodb server')
