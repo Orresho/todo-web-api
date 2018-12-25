8dab5b9343eb7937634d55416fcc93f6d6dae1c6
Orhan Hajderovic <orhanhajder@gmail.com>
2018-12-25 16:38:30 +0100

    add tests for get todos

--- server/server.js
+++ server/server.js
@@ -26,9 +26,9 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find()
-    .then(todos => {
-      res.send({ todos })
-    }, err => res.status(400).send(err))
+    .then(
+      todos => res.send({ todos }),
+      err => res.status(400).send(err));
 });
 
 app.listen(PORT, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,20 @@ const request = require('supertest');
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
+const todos = [
+  {
+    text: "first test todo"
+  },
+  {
+    text: "second test todo"
+  },
+];
+
 // Empty the todos before starting any of the test suites
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,8 +34,8 @@ describe('POST /todos', () => {
         if (err) {
           return done(err)
         }
-        
-        Todo.find().then((todos) => {
+
+        Todo.find({ text }).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -42,9 +53,21 @@ describe('POST /todos', () => {
           return done(err)
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(2)
           done();
         }).catch(e => done(e))
       })
+  });
+});
+
+describe('GET /todos', () => {
+  it('Should get all the todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done)
   });
 });
