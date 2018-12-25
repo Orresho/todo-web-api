7f4ce1ddafbffe80eeadc8fb4356aba36fea2b63
Orhan Hajderovic <orhanhajder@gmail.com>
2018-12-25 13:45:55 +0100

    test api

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save()
+    .then(doc => {
+      res.send(doc)
+    }, err => {
+      res.send(err)
+    })
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
