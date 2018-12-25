68c78f2badd4da35aad03aa1e07b0fe6ee3dfd74
Orhan Hajderovic <orhanhajder@gmail.com>
2018-12-25 16:26:10 +0100

    create a get req for todo

--- server/server.js
+++ server/server.js
@@ -24,6 +24,13 @@ app.post('/todos', (req, res) => {
     })
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos })
+    }, err => res.status(400).send(err))
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
