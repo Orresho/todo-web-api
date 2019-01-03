8b75fc63a21b2be6604afc9e4d7e8f96dd754618
Orhan Hajderovic <orhanhajder@gmail.com>
2019-01-03 17:21:08 +0100

    add update route for todo

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
@@ -61,6 +62,36 @@ app.delete('/todo/:id', (req, res) => {
       res.send({ todo });
     })
     .catch(err => res.status(400).send(err))
+});
+
+app.patch('/todo/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({ error: 'Invalid id', message: 'invalid objectID' })
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    // update completed
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  console.log(body);
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send({ error: 'Not found', message: 'Todo not found' })
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
 });
 
 app.listen(PORT, () => {
